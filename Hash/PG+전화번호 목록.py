c0c8fe34f229a7642e2405687d1fbf3208e23e7f
김태현 <taehyk528@gmail.com>
2021-09-15 00:04:50 +0900

    add: PG 전화번호 목록 (해시 - 정렬 + 문자열로 접근) - 딕셔너리로 처리할 수 있는 방법이 있을까?

---
+++ Hash/PG+전화번호 목록.py
@@ -0,0 +1,26 @@
+'''
+phone_book : 전화번호 배열
+어떤 번호(원소)가 다른 번호의 접두어인 경우가 있으면 T/F
+
+이 문제도 원소의 각자리를 비교하거나 해야 하는데 정렬이 필요할 것 같고 (짧 -> 긴 순서로)
+완전탐색으로 풀면 시간초과 나는 것 같아서 다른 방법 고민(생각안나면 완전탐색으로라도 접근)
+
+아이디어...
+1) 짧은것 단위로 끊어서 생각해봐도 좋을 것 같기도 하고
+119 976 119
+
+2) 정렬이 길이순이아니고 크기순이라면? 그럼 한 칸씩만 이동하면서 앞뒤로만 체크해도 될 것 같은데.
+119 120 136 976
+'''
+
+
+def solution(phone_book):
+    phone_book.sort()
+
+    # 반복문은 한 번만 돌면 됨
+    for i in range(len(phone_book) - 1):
+        # print(phone_book[i], phone_book[i+1][0: len(phone_book[i])])
+        if phone_book[i] == phone_book[i+1][0: len(phone_book[i])]:
+            return False
+
+    return True
