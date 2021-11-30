a98a653dfee34e048d30e013dfc8c5291e19713f
김태현 <taehyk528@gmail.com>
2021-11-30 22:34:45 +0900

    add : boj 10825 국영수 (정렬)

---
+++ Sort/boj10825.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+students = []
+
+n = int(input())
+for _ in range(n):
+    name, kor, eng, math = input().split()
+    students.append((name, int(kor), int(eng), int(math)))
+
+answer = sorted(students, key=lambda x: (-x[1], x[2], -x[3], x[0]))
+
+for student in answer:
+    print(student[0])
